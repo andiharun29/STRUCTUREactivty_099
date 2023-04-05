85ef8717db1ce7d8b9688e66d9a16879b8c653df
andiharun29 <andiharunalrasyid14@gmail.com>
2023-04-05 12:21:21 +0700

    masukkan data mahasiswa

--- STERUCTUREact3/STERUCTUREact3.cpp
+++ STERUCTUREact3/STERUCTUREact3.cpp
@@ -19,4 +19,18 @@ int main() {
 	mahasiswa mhs[20];
 	for (int i = 0; i < 3; i++)
 	{
-		
+		cout << "masukan nim =";
+		cin.getline(mhs[i].nim, 12);
+		cout << "masukan umur=";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << "masukan nama=";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat=";
+		cout << "\n\tMasukan Desa =";
+		cin.getline(mhs[i].alamat.Desa, 20);
+		cout << "\n\tMasukan Kota =";
+		cin.getline(mhs[i].alamat.Kota, 20);
+	}
+
+	
